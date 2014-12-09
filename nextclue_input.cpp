f49d0294317086cb5243e1a80bda905f86ec8393
Henry Garcia <hgarc014@ucr.edu>
2014-12-09 09:17:58 -0800

    removed outputclue

---
+++ nextclue_input.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "This file was useless, so I made it better..." << endl;
+    while(1);
+    never = 0;
+    return never;
+}
+
