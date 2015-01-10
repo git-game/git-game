31e82b3a6b6ccec37db44f9c6c7681fa9066e2a9
Henry Garcia <hgarc014@ucr.edu>
2015-01-09 18:09:24 -0800

    added check for OS

--- outputclue.sh
+++ outputclue.sh
@@ -8,18 +8,24 @@ file=$1
 bug=7c85d987a917c2a555d1391426978f05
 mesg="Linus has been here...\nI love messing with these amateur programmers!!\nIf you want some real fun, then you should try resolving a conflict between this branch (tree) and code4life.\nI introduced a little bug that you should fix in the conflict. >:)\nAfter you merge these 2 files you should run the shell script again!!\n\nGood Luck!!!"
 merges=$(git log --format=%h --merges | head -1)
+csum="md5sum"
+b64="base64 -d"
+if [[ "$OSTYPE" = "darwin" ]];then
+    csum="md5"
+    b64="base64 -D"
+fi
 
 if [ "$file" = "nextclue_input.cpp" ];then 
   if [ ${merges} ]; then 
     while read p; do 
       for w in $p;do 
-        if [ `echo $w | md5sum | awk '{print $1}'` = $bug ];then 
+        if [ `echo $w | $csum | awk '{print $1}'` = $bug ];then 
           echo -e $mesg; 
           exit; 
         fi; 
       done;
     done < $file ;
-    echo -e "Well, congratulations!! You fixed my conflict!!\nIf you would like to continue, then you should checkout to the $(echo bW91c2UK | base64 -d) branch!!\n" ;
+    echo -e "Well, congratulations!! You fixed my conflict!!\nIf you would like to continue, then you should checkout to the $(echo bW91c2UK | $b64 ) branch!!\n" ;
    else 
      echo -e $mesg; 
      exit;
